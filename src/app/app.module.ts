2ea96756e6853b238d252369a62629604fac4a2b
Jacobo <jacobogonzalezv@gmail.com>
2018-06-04 00:49:55 +0200

    instalados componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    RegistroComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
