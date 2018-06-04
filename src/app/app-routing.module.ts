0c1a0b5505e17fb616f799c8d48ffcb820ab9be2
Jacobo <jacobogonzalezv@gmail.com>
2018-06-04 15:53:10 +0200

    modificados los componentes y el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+   { path: 'home', component: HomeComponent },
+   { path: 'about', component: AboutComponent },
+   { path: 'login', component: LoginComponent },
+   { path: 'registro', component: RegistroComponent },
+   { path: 'administrador', component: AdminComponent },
+   { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
